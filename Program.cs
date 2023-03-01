43cb22f366143057e51a28ec48c3463436eb9726
Josef <swadijosef@gmail.com>
2023-03-01 22:18:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace D14_ovning1_uppgift8t15
+{
+    internal class Program
+    {
+        class Land
+        {
+            public string namn, styrestyp, huvudstad;
+            public int invånarantal;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
